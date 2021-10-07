c99e18e1d6e07a89d373d3c4e9a430ecddc97135
CyberBet4 <triumphanazia@gmail.com>
2021-10-07 10:10:02 +0100

    Fixed background colors

--- src/Screens/Login.js
+++ src/Screens/Login.js
@@ -10,7 +10,7 @@ import { faEnvelope, faLock } from '@fortawesome/free-solid-svg-icons';
 const Login = () => {
 
     return (
-        <div className="">
+        <div className="main">
             <div className="row">
                 <div className="col-12">
                     <div className="primary-bg mt-7 d-flex justify-content-center">
@@ -23,37 +23,39 @@ const Login = () => {
 
                     <h3 style={{color : "#fff"}} className="animate__animated animate__fadeIn large-text mt-4 text-center">Learn from anything and anywhere</h3>
 
-                    <div className="mt-5 bottom-sheet animate__animated animate__fadeInUp animate__faster pb-5">
-                        <form method="POST" action="#" >
-                            <div className="input-group mb-3">
-                                <span className="input-group-text">
-                                    <FontAwesomeIcon className="gray-color" icon={faEnvelope} />
-                                </span>
-                                <input type="email" placeholder="Email" className="form-control" />
-                            </div>
-                            
-                            <div className="input-group mb-3">
-                                <span className="input-group-text">
-                                    <FontAwesomeIcon className="gray-color" icon={faLock} />
-                                </span>
-                            <input type="password" name="" placeholder="Password" id="" className="form-control" />
-                            </div>
-                            
-                            {/* <button type="submit" className="w-100 mt-3 btn btn-primary"> */}
-                            <Link className="w-100 mt-3 btn btn-primary" to="/home">Sign In</Link>
-                                {/* Sign In */}
+                    <div className="d-flex justify-content-center">
+                        <div className="mt-5 bottom-sheet animate__animated animate__fadeInUp animate__faster pb-5">
+                            <form method="POST" action="#" >
+                                <div className="input-group mb-3">
+                                    <span className="input-group-text">
+                                        <FontAwesomeIcon className="gray-color" icon={faEnvelope} />
+                                    </span>
+                                    <input type="email" placeholder="Email" className="form-control" />
+                                </div>
+                                
+                                <div className="input-group mb-3">
+                                    <span className="input-group-text">
+                                        <FontAwesomeIcon className="gray-color" icon={faLock} />
+                                    </span>
+                                <input type="password" name="" placeholder="Password" id="" className="form-control" />
+                                </div>
+                                
+                                {/* <button type="submit" className="w-100 mt-3 btn btn-primary"> */}
+                                <Link className="w-100 mt-3 btn btn-primary" to="/home">Sign In</Link>
+                                    {/* Sign In */}
 
-                            {/* </button> */}
+                                {/* </button> */}
 
-                            <button type="submit" className="w-100 mt-3 btn btn-secondary">
-                                Sign in with Google 
-                            </button>
+                                <button type="submit" className="w-100 mt-3 btn btn-secondary">
+                                    Sign in with Google 
+                                </button>
 
-                        </form>
-                        <p className="text-center base-text mt-3">
-                            New Here? 
-                            <Link className="link" to="/signup"> Create an account</Link>
-                        </p>
+                            </form>
+                            <p className="text-center base-text mt-3">
+                                New Here? 
+                                <Link className="link" to="/signup"> Create an account</Link>
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
--- src/Screens/Signup.js
+++ src/Screens/Signup.js
@@ -7,50 +7,54 @@ import { faEnvelope, faLock } from '@fortawesome/free-solid-svg-icons';
 
 const Signup = () => {
     return (
-        <div className="row">
-            <div className="col-12">
-                <div className="primary-bg mt-7 d-flex justify-content-center">
-                    <img src={logo} className="" alt="Home Tutor Logo" />
-                </div>
+        <div className="main">
+            <div className="row">
+                <div className="col-12">
+                    <div className="primary-bg mt-7 d-flex justify-content-center">
+                        <img src={logo} className="" alt="Home Tutor Logo" />
+                    </div>
 
-                <h3 className="big-display-alt mt-5 text-center " style={{color: "#ffffff"}}>
-                    Home Tutor
-                </h3>
+                    <h3 className="big-display-alt mt-5 text-center " style={{color: "#ffffff"}}>
+                        Home Tutor
+                    </h3>
 
-                <h3 style={{color : "#fff"}} className=" large-text mt-4 text-center">Learn from anything and anywhere</h3>
+                    <h3 style={{color : "#fff"}} className=" large-text mt-4 text-center">Learn from anything and anywhere</h3>
 
-                <div className="mt-5 bottom-sheet animate__animated animate__fadeInUp animate__faster pb-5" id="signup-sheet">
-                    <form method="POST" action="#" >
-                        <div className="input-group mb-3">
-                            <span className="input-group-text">
-                                <FontAwesomeIcon className="gray-color" icon={faEnvelope} />
-                            </span>
-                            <input type="email" placeholder="Email" className="form-control" />
-                        </div>
-                        
-                        <div className="input-group mb-3">
-                            <span className="input-group-text">
-                                <FontAwesomeIcon className="gray-color" icon={faLock} />
-                            </span>
-                            <input type="password" placeholder="Password" className="form-control" />
-                        </div>
+                    <div className="d-flex justify-content-center">
+                    <div className="mt-5 bottom-sheet animate__animated animate__fadeInUp animate__faster pb-5" id="signup-sheet">
+                        <form method="POST" action="#" >
+                            <div className="input-group mb-3">
+                                <span className="input-group-text">
+                                    <FontAwesomeIcon className="gray-color" icon={faEnvelope} />
+                                </span>
+                                <input type="email" placeholder="Email" className="form-control" />
+                            </div>
+                            
+                            <div className="input-group mb-3">
+                                <span className="input-group-text">
+                                    <FontAwesomeIcon className="gray-color" icon={faLock} />
+                                </span>
+                                <input type="password" placeholder="Password" className="form-control" />
+                            </div>
 
-                        <div className="input-group mb-3">
-                            <span className="input-group-text">
-                                <FontAwesomeIcon className="gray-color" icon={faLock} />
-                            </span>
-                            <input type="password" placeholder="Confirm Password" className="form-control" />
-                        </div>
-                        
-                        <button type="submit" className="w-100 mt-3 btn btn-primary">
-                            Create Account
-                        </button>
+                            <div className="input-group mb-3">
+                                <span className="input-group-text">
+                                    <FontAwesomeIcon className="gray-color" icon={faLock} />
+                                </span>
+                                <input type="password" placeholder="Confirm Password" className="form-control" />
+                            </div>
+                            
+                            <button type="submit" className="w-100 mt-3 btn btn-primary">
+                                Create Account
+                            </button>
 
-                    </form>
-                    <p className="text-center base-text mt-3">
-                        Already a Member? 
-                        <Link className="link" to="/"> Sign In</Link>
-                    </p>
+                        </form>
+                        <p className="text-center base-text mt-3">
+                            Already a Member? 
+                            <Link className="link" to="/"> Sign In</Link>
+                        </p>
+                    </div>
+                    </div>
                 </div>
             </div>
         </div>
